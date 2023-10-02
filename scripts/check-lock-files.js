351a3b1e3645ddb6164327880fae7ce241a56793
moatorres <44585769+moatorres@users.noreply.github.com>
2023-10-02 00:11:15 -0300

    chore(repo): add script to check lock files
    prevents invalid lock files to be pushed to the repository

---
+++ scripts/check-lock-files.js
@@ -0,0 +1,45 @@
+/**
+ * @fileoverview Checks that the lock file is valid.
+ * @module scripts/check-lock-file
+ */
+
+const fs = require('fs')
+
+const INVALID_LOCK_FILE =
+  '❌ Invalid occurence of "package-lock.json" or "yarn.lock" files. Please remove it and use only "pnpm-lock.yaml"'
+const INVALID_PNPM_LOCK_FILE =
+  '❌ The "pnpm-lock.yaml" has reference to local repository ("localhost:4873"). Please use ensure you disable local registry before running "pnpm install"'
+
+const INVALID_TARBALL =
+  '❌ The "pnpm-lock.yaml" has reference to tarball package. Please use npm registry only'
+
+const MISSING_PNPM_LOCK_FILE = '❌ The "pnpm-lock.yaml" does not exist or cannot be read'
+
+function checkLockFiles() {
+  const errors = []
+
+  if (fs.existsSync('package-lock.json') || fs.existsSync('yarn.lock')) {
+    errors.push(INVALID_LOCK_FILE)
+  }
+
+  try {
+    const content = fs.readFileSync('pnpm-lock.yaml', 'utf-8')
+    if (content.match(/localhost:487/)) errors.push(INVALID_PNPM_LOCK_FILE)
+    if (content.match(/resolution: \{tarball/)) errors.push(INVALID_TARBALL)
+  } catch {
+    errors.push(MISSING_PNPM_LOCK_FILE)
+  }
+  return errors
+}
+
+console.log('🔒 Validating lock files\n')
+
+const invalid = checkLockFiles()
+
+if (invalid.length > 0) {
+  invalid.forEach((e) => console.log(e))
+  process.exit(1)
+} else {
+  console.log('✅ Lock file is valid\n')
+  process.exit(0)
+}
