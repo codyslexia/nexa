ed1226825e3585066637a982f180b72c0d5d415f
moatorres <44585769+moatorres@users.noreply.github.com>
2023-10-07 12:54:44 -0300

    feat(ui): add base layout and styles
    creates and configures nexa's frontend base layout, sitemap, providers and home page

---
+++ apps/frontend/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="nexa">
+      <a href="https://github.com/codyslexia/nexa">codyslexia/nexa</a>
+    </div>
+  )
+}
